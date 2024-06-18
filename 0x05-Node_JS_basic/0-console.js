523894168159c7dbb72101bf7d2b24865eb8876e
gameliaprogrammer <eng.gameliaahmed@gmail.com>
2024-06-18 22:08:30 +0000

    add files

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author gamelia programmer <https://github.com/gameliaprogrammer>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
